c9978316c1ca31c6fc7b64abc34dc6a579f1d8c9
ChimpGamer <Tim.Cremers@Ziggo.nl>
2025-09-07 18:24:40 +0200

    Bump Kotlin version to 2.2.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import java.util.*
 val exposedVersion = "0.60.0"
 
 plugins {
-    kotlin("jvm") version "2.2.0"
+    kotlin("jvm") version "2.2.10"
     id("com.gradleup.shadow") version "8.3.5"
     `maven-publish`
 }
