5807c794a684f5f9089189e5f47833f62615cb41
ChimpGamer <Tim.Cremers@Ziggo.nl>
2025-10-12 13:04:49 +0200

    Bump cloud dependencies to version 2.0.0-beta.13

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
     compileOnly("io.github.miniplaceholders:miniplaceholders-api:3.0.1")
     compileOnly("io.github.miniplaceholders:miniplaceholders-kotlin-ext:3.0.1")
 
-    compileOnly("org.incendo:cloud-paper:2.0.0-beta.10")
+    compileOnly("org.incendo:cloud-paper:2.0.0-beta.13")
 
     compileOnly("dev.rosewood:rosestacker:1.5.30")
     compileOnly("com.arcaniax:HeadDatabase-API:1.3.2")
