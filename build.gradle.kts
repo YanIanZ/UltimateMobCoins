93fda03805b986b37697ed9be6a6815045d19c88
ChimpGamer <19960733+ChimpGamer@users.noreply.github.com>
2026-05-27 18:33:59 +0200

    Bump kotlin to 2.3.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ import java.util.*
 val exposedVersion = "0.60.0"
 
 plugins {
-    kotlin("jvm") version "2.2.21"
-    id("com.gradleup.shadow") version "9.3.0"
+    kotlin("jvm") version "2.3.21"
+    id("com.gradleup.shadow") version "9.4.1"
     `maven-publish`
 }
 
